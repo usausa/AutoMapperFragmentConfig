1b25c973046011ead619bb505fd9b1674c65db4b
usausa <machi.pon@gmail.com>
2025-12-06 12:04:52 +0900

    Update generator

--- AutoMapperFragmentConfig.Generator/Diagnostics.cs
+++ AutoMapperFragmentConfig.Generator/Diagnostics.cs
@@ -4,7 +4,7 @@ using Microsoft.CodeAnalysis;
 
 internal static class Diagnostics
 {
-    public static DiagnosticDescriptor InvalidExtensionMethodDefinition => new(
+    public static DiagnosticDescriptor InvalidExtensionMethodDefinition { get; } = new(
         id: "AMFC0001",
         title: "Invalid extension method definition",
         messageFormat: "Extension method must be static void. method=[{0}]",
@@ -12,7 +12,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor InvalidExtensionMethodParameter => new(
+    public static DiagnosticDescriptor InvalidExtensionMethodParameter { get; } = new(
         id: "AMFC0002",
         title: "Invalid extension method parameter",
         messageFormat: "Parameter type must be IMapperConfigurationExpression and IServiceProvider(option). method=[{0}]",
@@ -20,7 +20,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor InvalidConfigMethodDefinition => new(
+    public static DiagnosticDescriptor InvalidConfigMethodDefinition { get; } = new(
         id: "AMFC0003",
         title: "Invalid config method definition",
         messageFormat: "Config method must be static void. method=[{0}]",
@@ -28,7 +28,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor InvalidConfigMethodParameter => new(
+    public static DiagnosticDescriptor InvalidConfigMethodParameter { get; } = new(
         id: "AMFC0004",
         title: "Invalid config method parameter",
         messageFormat: "Parameter type must be IProfileExpression and IServiceProvider(option). method=[{0}]",
@@ -36,7 +36,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor ProviderParameterRequired => new(
+    public static DiagnosticDescriptor ProviderParameterRequired { get; } = new(
         id: "AMFC0005",
         title: "IServiceProvider parameter required",
         messageFormat: "IServiceProvider parameter required for extension. method=[{0}]",
